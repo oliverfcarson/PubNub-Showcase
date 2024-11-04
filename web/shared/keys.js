f3f9a9e84f5386b7c035a5e899a42db9956b7134
oliverfcarson <oliver.carson@pubnub.com>
2024-11-04 11:38:57 -0800

    Project Structure Cleanup Part 2
    -Removal of other unnecessary files for chat app

--- web/shared/keys.js
+++
@@ -1,8 +0,0 @@
-/**
- * Populate this file with both your PubNub and any additional keys (e.g. Google API keys)
- * Please see the ReadMe file for details of where to obtain keys
- */
-
-const publish_key = '' //  ENTER YOUR PUBLISH KEY HERE
-const subscribe_key = '' //  ENTER YOUR SUBSCRIBE KEY HERE
-const geo_key = ''  //  ENTER YOUR GOOGLE API KEY HERE WITH THE FOLLOWING ENABLED: Maps JavaScript API, Geocoding API
